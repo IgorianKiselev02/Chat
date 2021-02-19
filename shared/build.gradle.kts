38867f1a5dd81743f5a150e0b6388499c6fc6237
IgorianKiselev02 <kigorleo@gmail.com>
2021-02-19 21:07:20 +0300

    Fix gradle v2

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -3,6 +3,8 @@ group = "ru.senin.kotlin.net"
 dependencies {
     implementation("com.fasterxml.jackson.core:jackson-annotations:2.10.2")
 
+    implementation("org.jetbrains.exposed:exposed-core:0.29.1")
+
     val ktor_version: String by project
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
 }
